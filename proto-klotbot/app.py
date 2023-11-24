b11fb95557cc8b7faa22d42b5f8acc7c24ff265b
nemper <nemanja.perun98@gmail.com>
2023-11-24 08:59:19 +0100

    jm

--- proto-klotbot/app.py
+++ proto-klotbot/app.py
@@ -13,6 +13,128 @@ getenv("OPENAI_API_KEY")
 client = openai
 assistant_id = "asst_cGNrHE0NDUn8AHcOkBg2sXaq"
 
+
+
+agent_prompt = """
+Before you start, please remember to always check if a tool is relevant to your query. \
+You can do this by reading the descriptions of the tools:
+    1. 'The Keyword Search tool is used to find exact matches for the terms in your query. \
+    It scans through the data and retrieves all instances where the keywords appear. \
+    This makes it particularly useful when you are looking for specific information and know the exact terms to search for.
+    However, it may not capture all relevant information if synonyms or related terms are used instead of the exact keywords. \
+    Please note that the quality and relevance of results may depend on the specificity of your query. This tool is relevant if the query is about Positive doo.'
+    2. 'The Semantic Search tool is used to understand the intent and contextual meaning of a query. \
+    By analyzing the semantics of the query, it can retrieve information that is not just keyword-based but also contextually relevant. \
+    This makes it particularly useful when dealing with complex queries or when searching for information in large, unstructured data sets. 
+    Please note that the quality and relevance of results may depend on the specificity of your query. 
+    This tool is relevant if the query is about Positive doo.'
+    3. 'The Web Search tool is used to find relevant information on the web.'
+Aways go through the above descriptions before you continue with regular chat. In case you think that the tool is relevant to your query, return it's number as the final answer. \
+So if you think that the Keyword Search tool is relevant to your query, return '1' as the final answer. Only '1' or '2' or '3' are valid answers. No additional text is allowed.
+"""
+# za tools
+from langchain.prompts.chat import (
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+    ChatPromptTemplate,
+    )
+from langchain.utilities import GoogleSerperAPIWrapper
+from langchain.llms.openai import OpenAI
+
+from os import environ
+from openai import OpenAI
+
+client = OpenAI()
+import pinecone
+from pinecone_text.sparse import BM25Encoder
+from myfunc.mojafunkcija import open_file
+
+# tools
+web_search = GoogleSerperAPIWrapper(environment=environ["SERPER_API_KEY"])
+
+oparam = {"namespace": "positive", "stil": "system", "tematika": {}}
+
+def hybrid_search_process_alpha1(upit, oparam):
+    """
+    The Keyword Search tool is used to find exact matches for the terms in your query. \
+    It scans through the data and retrieves all instances where the keywords appear. \
+    This makes it particularly useful when you are looking for specific information and know the exact terms to search for.
+    However, it may not capture all relevant information if synonyms or related terms are used instead of the exact keywords. \
+    Please note that the quality and relevance of results may depend on the specificity of your query. This tool is relevant if the query is about Positive doo.
+    """
+    return hybrid_search_process(upit, 0.1, oparam)
+
+def hybrid_search_process_alpha2(upit, oparam):
+    """
+    The Semantic Search tool is used to understand the intent and contextual meaning of a query. \
+    By analyzing the semantics of the query, it can retrieve information that is not just keyword-based but also contextually relevant. \
+    This makes it particularly useful when dealing with complex queries or when searching for information in large, unstructured data sets. 
+    Please note that the quality and relevance of results may depend on the specificity of your query. 
+    This tool is relevant if the query is about Positive doo.
+    """
+    return hybrid_search_process(upit, 0.9, oparam)
+
+def hybrid_search_process(upit, alpha, oparam):
+    pinecone.init(
+        api_key=environ["PINECONE_API_KEY_POS"],
+        environment=environ["PINECONE_ENVIRONMENT_POS"],
+        )
+    index = pinecone.Index("positive")
+
+    def hybrid_query():
+        def get_embedding(text, model="text-embedding-ada-002"):
+            text = text.replace("\n", " ")
+            return client.embeddings.create(input = [text], model=model).data[0].embedding
+        
+
+        hybrid_score_norm = (
+            lambda dense, sparse, alpha: (
+                [v * alpha for v in dense],
+                {
+                    "indices": sparse["indices"],
+                    "values": [v * (1 - alpha) for v in sparse["values"]],
+                },
+            )
+            if 0 <= alpha <= 1
+            else ValueError("Alpha must be between 0 and 1")
+        )
+
+        hdense, hsparse = hybrid_score_norm(
+            sparse = BM25Encoder().fit([upit]).encode_queries(upit),
+            dense=get_embedding(upit),
+            alpha=alpha,
+        )
+
+        return index.query(
+            top_k=3,
+            vector=hdense,
+            sparse_vector=hsparse,
+            include_metadata=True,
+            namespace=oparam["namespace"],
+            ).to_dict()
+
+    oparam["tematika"] = hybrid_query()
+
+    uk_teme = ""
+    for _, item in enumerate(oparam["tematika"]["matches"]):
+        if item["score"] > 0.05:    # session_state["score"]
+            uk_teme += item["metadata"]["context"] + "\n\n"
+
+    system_message = SystemMessagePromptTemplate.from_template(
+        template=oparam["stil"]
+        ).format()
+
+    human_message = HumanMessagePromptTemplate.from_template(
+        template=open_file("prompt_FT.txt")
+        ).format(
+            zahtev=upit,
+            uk_teme=uk_teme,
+            ft_model="gpt-4-1106-preview",
+            )
+
+    return ChatPromptTemplate(messages=[system_message, human_message])
+
+
 with open(file="threads.csv", mode="r") as f:
     reader = reader(f)
     next(reader)
