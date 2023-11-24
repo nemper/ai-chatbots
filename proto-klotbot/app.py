94e04c33055bd96d25f7780fd315cbaa3c598313
nemper <nemanja.perun98@gmail.com>
2023-11-24 10:02:04 +0100

    gee

--- proto-klotbot/app.py
+++ proto-klotbot/app.py
@@ -49,90 +49,64 @@ import pinecone
 from pinecone_text.sparse import BM25Encoder
 from myfunc.mojafunkcija import open_file
 
-# tools
-web_search = GoogleSerperAPIWrapper(environment=environ["SERPER_API_KEY"])
-
-oparam = {"namespace": "positive", "stil": "system", "tematika": {}}
-
-def hybrid_search_process_alpha1(upit, oparam):
-    """
-    The Keyword Search tool is used to find exact matches for the terms in your query. \
-    It scans through the data and retrieves all instances where the keywords appear. \
-    This makes it particularly useful when you are looking for specific information and know the exact terms to search for.
-    However, it may not capture all relevant information if synonyms or related terms are used instead of the exact keywords. \
-    Please note that the quality and relevance of results may depend on the specificity of your query. This tool is relevant if the query is about Positive doo.
-    """
-    return hybrid_search_process(upit, 0.1, oparam)
-
-def hybrid_search_process_alpha2(upit, oparam):
-    """
-    The Semantic Search tool is used to understand the intent and contextual meaning of a query. \
-    By analyzing the semantics of the query, it can retrieve information that is not just keyword-based but also contextually relevant. \
-    This makes it particularly useful when dealing with complex queries or when searching for information in large, unstructured data sets. 
-    Please note that the quality and relevance of results may depend on the specificity of your query. 
-    This tool is relevant if the query is about Positive doo.
-    """
-    return hybrid_search_process(upit, 0.9, oparam)
-
-def hybrid_search_process(upit, alpha, oparam):
-    pinecone.init(
-        api_key=environ["PINECONE_API_KEY_POS"],
-        environment=environ["PINECONE_ENVIRONMENT_POS"],
-        )
-    index = pinecone.Index("positive")
-
-    def hybrid_query():
-        def get_embedding(text, model="text-embedding-ada-002"):
-            text = text.replace("\n", " ")
-            return client.embeddings.create(input = [text], model=model).data[0].embedding
-        
-
-        hybrid_score_norm = (
-            lambda dense, sparse, alpha: (
-                [v * alpha for v in dense],
-                {
-                    "indices": sparse["indices"],
-                    "values": [v * (1 - alpha) for v in sparse["values"]],
+if "assistant" not in st.session_state:
+    st.session_state.assistant = client.beta.assistants.create(
+        instructions="You are a knowledge retrieval AI assistant. Use your knowledge base and uploaded files to best respond to user queries.",
+        model="gpt-4-1106-preview",
+        tools=[
+            {
+            "type": "function",
+            "function": {
+            "name": "web_search",
+            "description": """This tool uses Google Search to find the most relevant and up-to-date information on the web. \
+                This tool is particularly useful when you need comprehensive information on a specific topic, \
+                    want to explore different viewpoints, or are looking for the latest news and data.
+                    Please note that the quality and relevance of results may depend on the specificity of your query. \
+                        Never use this tool when asked about Positive doo.""",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "location": {"type": "string", "description": "The city and state e.g. San Francisco, CA"},
+                    "unit": {"type": "string", "enum": ["c", "f"]}
                 },
-            )
-            if 0 <= alpha <= 1
-            else ValueError("Alpha must be between 0 and 1")
-        )
+                "required": ["location"]
+            }}}, 
+            {
+            "type": "function",
+            "function": {
+            "name": "keyword_search",
+            "description": """The Keyword Search tool is used to find exact matches for the terms in your query. \
+                It scans through the data and retrieves all instances where the keywords appear. \
+                    This makes it particularly useful when you are looking for specific information and know the exact terms to search for.
+                    However, it may not capture all relevant information if synonyms or related terms are used instead of the exact keywords. \
+                        Please note that the quality and relevance of results may depend on the specificity of your query. \
+                            This tool is relevant if the query is about Positive doo.""",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                "location": {"type": "string", "description": "The city and state e.g. San Francisco, CA"},
+                },
+                "required": ["location"]
+            }}}, 
+            {
+            "type": "function",
+            "function": {
+            "name": "semantic_search",
+            "description": """The Semantic Search tool is used to understand the intent and contextual meaning of a query. \
+                By analyzing the semantics of the query, it can retrieve information that is not just keyword-based but also contextually relevant. \
+                    This makes it particularly useful when dealing with complex queries or when searching for information in large, unstructured data sets. 
+                    Please note that the quality and relevance of results may depend on the specificity of your query. 
+                    This tool is relevant if the query is about Positive doo.""",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                "location": {"type": "string", "description": "The city and state e.g. San Francisco, CA"},
+                },
+                "required": ["location"]
+            }}}
+            ])
 
-        hdense, hsparse = hybrid_score_norm(
-            sparse = BM25Encoder().fit([upit]).encode_queries(upit),
-            dense=get_embedding(upit),
-            alpha=alpha,
-        )
 
-        return index.query(
-            top_k=3,
-            vector=hdense,
-            sparse_vector=hsparse,
-            include_metadata=True,
-            namespace=oparam["namespace"],
-            ).to_dict()
-
-    oparam["tematika"] = hybrid_query()
-
-    uk_teme = ""
-    for _, item in enumerate(oparam["tematika"]["matches"]):
-        if item["score"] > 0.05:    # session_state["score"]
-            uk_teme += item["metadata"]["context"] + "\n\n"
-
-    system_message = SystemMessagePromptTemplate.from_template(
-        template=oparam["stil"]
-        ).format()
-
-    human_message = HumanMessagePromptTemplate.from_template(
-        template=open_file("prompt_FT.txt")
-        ).format(
-            zahtev=upit,
-            uk_teme=uk_teme,
-            ft_model="gpt-4-1106-preview",
-            )
-
-    return ChatPromptTemplate(messages=[system_message, human_message])
 
 
 with open(file="threads.csv", mode="r") as f:
