2ee837eac2ddbab97c855a9c459661f70e718f3a
nemper <nemanja.perun98@gmail.com>
2023-12-07 12:23:43 +0100

    sss

--- proto-klotbot/positive_zapisnik.py
+++ proto-klotbot/positive_zapisnik.py
@@ -208,19 +208,20 @@ def main():
     """
     run = None
 
-    with st.form(key="my_form", clear_on_submit=False):
-        prompt = st.text_area(
-            label="Unesite pitanje",
-            key="prompt_prva",
-            height=100)
-        if st.form_submit_button(label="Submit"):
-            if st.session_state.thread_id is not None:
-                client.beta.threads.messages.create(thread_id=st.session_state.thread_id, role="user", content=prompt) 
-                run = client.beta.threads.runs.create(thread_id=st.session_state.thread_id, assistant_id=assistant.id, 
-                                    instructions=instructions)
-                sleep(1)
-            else:
-                st.warning("Molimo Vas da izaberete postojeci ili da kreirate novi chat.")
+    if st.session_state.thread_id is not None:
+        with st.form(key="my_form", clear_on_submit=False):
+            prompt = st.text_area(
+                label="Unesite pitanje",
+                key="prompt_prva",
+                height=100)
+            if st.form_submit_button(label="Submit") and prompt is not None:
+                if st.session_state.thread_id is not None:
+                    client.beta.threads.messages.create(thread_id=st.session_state.thread_id, role="user", content=prompt) 
+                    run = client.beta.threads.runs.create(thread_id=st.session_state.thread_id, assistant_id=assistant.id, 
+                                        instructions=instructions)
+                    sleep(1)
+                else:
+                    st.warning("Molimo Vas da izaberete postojeci ili da kreirate novi chat.")
     
 
     # ako se poziva neka funkcija
