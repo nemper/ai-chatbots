2f171cc3bd28c34b0198e12134360471ea2b0f57
nemper <nemanja.perun98@gmail.com>
2024-04-17 09:02:33 +0200

    work vars dict

---
+++ old-custom-lib/myfunc/varvars_dicts.py
@@ -0,0 +1,15 @@
+work_vars = {
+    "keys" : {
+        "openai_key": "sk-1234567890abcdef1234567890abcdef",
+        "openai_key2": "sk-1234567890abcdef1234567890abcdef",
+        "openai_key3": "sk-1234567890abcdef1234567890abcdef",
+        },
+
+    "names" : {
+        "openai_model": "gpt-4-turbo",
+        },
+
+    "addresses" : {
+        "openai_address": "https://api.openai.com/v1/engines/gpt-4-turbo/completions",
+        },
+    }
