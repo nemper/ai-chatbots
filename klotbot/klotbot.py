689464c8524c24cf2854cebbc1be31604e07f86d
nemper <nemanja.perun98@gmail.com>
2024-05-08 08:36:59 +0200

    count tokens

--- klotbot/klotbot.py
+++ klotbot/klotbot.py
@@ -4,16 +4,19 @@ import streamlit as st
 import uuid
 
 from openai import OpenAI
-
+from pinecone import Pinecone
+from pinecone_text.sparse import BM25Encoder
 from myfunc.asistenti import read_aad_username
 from myfunc.mojafunkcija import positive_login
 from myfunc.prompts import ConversationDatabase, PromptDatabase
-from myfunc.retrievers import HybridQueryProcessor
+# from myfunc.retrievers import HybridQueryProcessor
 from myfunc.varvars_dicts import work_vars
+from langchain_openai import ChatOpenAI
+from langchain_community.callbacks import get_openai_callback
 
-
+import tiktoken
+openai_api_key = os.environ.get("OPENAI_API_KEY")
 client=OpenAI()
-processor = HybridQueryProcessor() # namespace moze i iz env
 
 try:
     x = st.session_state.sys_ragbot
@@ -22,12 +25,322 @@ except:
         prompt_map = db.get_prompts_by_names(["rag_answer_reformat", "sys_ragbot"],[os.getenv("RAG_ANSWER_REFORMAT"), os.getenv("SYS_RAGBOT")])
         st.session_state.rag_answer_reformat = prompt_map.get("rag_answer_reformat", "You are helpful assistant")
         st.session_state.sys_ragbot = prompt_map.get("sys_ragbot", "You are helpful assistant")
+
+
+
+class HybridQueryProcessor:
+    """
+    A processor for executing hybrid queries using Pinecone.
+
+    This class allows the execution of queries that combine dense and sparse vector searches,
+    typically used for retrieving and ranking information based on text data.
+
+    Attributes:
+        api_key (str): The API key for Pinecone.
+        environment (str): The Pinecone environment setting.
+        alpha (float): The weight used to balance dense and sparse vector scores.
+        score (float): The score treshold.
+        index_name (str): The name of the Pinecone index to be used.
+        index: The Pinecone index object.
+        namespace (str): The namespace to be used for the Pinecone index.
+        top_k (int): The number of results to be returned.
+            
+    Example usage:
+    processor = HybridQueryProcessor(api_key=environ["PINECONE_API_KEY"], 
+                                 environment=environ["PINECONE_API_KEY"],
+                                 alpha=0.7, 
+                                 score=0.35,
+                                 index_name='custom_index'), 
+                                 namespace=environ["NAMESPACE"],
+                                 top_k = 10 # all params are optional
+
+    result = processor.hybrid_query("some query text")    
+    """
+
+    def __init__(self, **kwargs):
+        """
+        Initializes the HybridQueryProcessor with optional parameters.
+
+        The API key and environment settings are fetched from the environment variables.
+        Optional parameters can be passed to override these settings.
+
+        Args:
+            **kwargs: Optional keyword arguments:
+                - api_key (str): The API key for Pinecone (default fetched from environment variable).
+                - environment (str): The Pinecone environment setting (default fetched from environment variable).
+                - alpha (float): Weight for balancing dense and sparse scores (default 0.5).
+                - score (float): Weight for balancing dense and sparse scores (default 0.05).
+                - index_name (str): Name of the Pinecone index to be used (default 'positive').
+                - namespace (str): The namespace to be used for the Pinecone index (default fetched from environment variable).
+                - top_k (int): The number of results to be returned (default 6).
+        """
+        self.api_key = kwargs.get('api_key', os.getenv('PINECONE_API_KEY'))
+        self.environment = kwargs.get('environment', os.getenv('PINECONE_API_KEY'))
+        self.alpha = kwargs.get('alpha', 0.5)  # Default alpha is 0.5
+        self.score = kwargs.get('score', 0.05)  # Default score is 0.05
+        self.index_name = kwargs.get('index', 'neo-positive')  # Default index is 'positive'
+        self.namespace = kwargs.get('namespace', os.getenv("NAMESPACE"))  
+        self.top_k = kwargs.get('top_k', 6)  # Default top_k is 6
+        self.index = None
+        self.host = os.getenv("PINECONE_HOST")
+        self.init_pinecone()
+
+    def init_pinecone(self):
+        """
+        Initializes the Pinecone connection and index.
+        """
+        pinecone=Pinecone(api_key=self.api_key, host=self.host)
+        self.index = pinecone.Index(host=self.host)
+
+    def get_embedding(self, text, model="text-embedding-3-large"):
+
+        """
+        Retrieves the embedding for the given text using the specified model.
+
+        Args:
+            text (str): The text to be embedded.
+            model (str): The model to be used for embedding. Default is "text-embedding-3-large".
+
+        Returns:
+            list: The embedding vector of the given text.
+            int: The number of prompt tokens used.
+        """
+        
+        text = text.replace("\n", " ")
+        prompt_tokens = client.embeddings.create(input=[text], model=model).usage.prompt_tokens
+        result = client.embeddings.create(input=[text], model=model).data[0].embedding
+       
+        return result, prompt_tokens
+
+
+    def hybrid_score_norm(self, dense, sparse):
+        """
+        Normalizes the scores from dense and sparse vectors using the alpha value.
+
+        Args:
+            dense (list): The dense vector scores.
+            sparse (dict): The sparse vector scores.
+
+        Returns:
+            tuple: Normalized dense and sparse vector scores.
+        """
+        return ([v * self.alpha for v in dense], 
+                {"indices": sparse["indices"], 
+                 "values": [v * (1 - self.alpha) for v in sparse["values"]]})
     
+    def hybrid_query(self, upit, top_k=None, filter=None, namespace=None):
+        # Get embedding and unpack results
+        dense, prompt_tokens = self.get_embedding(text=upit)
+
+        # Use those results in another function call
+        hdense, hsparse = self.hybrid_score_norm(
+            sparse=BM25Encoder().fit([upit]).encode_queries(upit),
+            dense=dense
+        )
+
+        query_params = {
+            'top_k': top_k or self.top_k,
+            'vector': hdense,
+            'sparse_vector': hsparse,
+            'include_metadata': True,
+            'namespace': namespace or self.namespace
+        }
+
+        if filter:
+            query_params['filter'] = filter
+
+        response = self.index.query(**query_params)
+
+        matches = response.to_dict().get('matches', [])
+
+        results = []
+        for match in matches:
+            metadata = match.get('metadata', {})
+            context = metadata.get('context', '')
+            chunk = metadata.get('chunk')
+            source = metadata.get('source')
+            try:
+                score = match.get('score', 0)
+            except:
+                score = metadata.get('score', 0)
+            if context:
+                results.append({"page_content": context, "chunk": chunk, "source": source, "score": score})
+        
+        return results, prompt_tokens  # Also return prompt_tokens
+       
+
+
+    def process_query_results(self, upit):
+        """
+        Processes the query results and prompt tokens based on relevance score and formats them for a chat or dialogue system.
+        Additionally, returns a list of scores for items that meet the score threshold.
+        """
+        tematika, prompt_tokens = self.hybrid_query(upit)  # Also retrieve prompt_tokens
+
+        uk_teme = ""
+        score_list = []
+        for item in tematika:
+             if item["score"] > self.score:
+                uk_teme += item["page_content"] + "\n\n"
+                score_list.append(item["score"])
+        
+        return uk_teme, score_list, prompt_tokens  # Return prompt_tokens along with other results
+    
+    def process_query_results_dict(self, upit):
+        """
+        Processes the query results and prompt tokens based on relevance score and formats them for a chat or dialogue system.
+        
+        """
+        tematika, prompt_tokens = self.hybrid_query(upit)  # Also retrieve prompt_tokens
+
+        score_list = []
+        
+        return tematika, score_list, prompt_tokens  # Return prompt_tokens along with other results
+
+    def process_query_parent_results(self, upit):
+        """
+        Processes the query results and returns top result with source name, chunk number, and page content.
+        It is used for parent-child queries.
+
+        Args:
+            upit (str): The original query text.
+    
+        Returns:
+            tuple: Formatted string for chat prompt, source name, and chunk number.
+        """
+        tematika, prompt_tokens = self.hybrid_query(upit)
+
+        # Check if there are any matches
+        if not tematika:
+            return "No results found", None, None
+
+        # Extract information from the top result
+        top_result = tematika[0]
+        top_context = top_result.get('page_content', '')
+        top_chunk = top_result.get('chunk')
+        top_source = top_result.get('source')
+
+        return top_context, top_source, top_chunk, prompt_tokens
+
+     
+    def search_by_source(self, upit, source_result, top_k=5, filter=None):
+        """
+        Perform a similarity search for documents related to `upit`, filtered by a specific `source_result`.
+        
+        :param upit: Query string.
+        :param source_result: source to filter the search results.
+        :param top_k: Number of top results to return.
+        :param filter: Additional filter criteria for the query.
+        :return: Concatenated page content of the search results.
+        """
+        filter_criteria = filter or {}
+        filter_criteria['source'] = source_result
+        top_k = top_k or self.top_k
+        
+        doc_result, prompt_tokens = self.hybrid_query(upit, top_k=top_k, filter=filter_criteria, namespace=self.namespace)
+        result = "\n\n".join(document['page_content'] for document in doc_result)
+    
+        return result, prompt_tokens
+        
+       
+    def search_by_chunk(self, upit, source_result, chunk, razmak=3, top_k=20, filter=None):
+        """
+        Perform a similarity search for documents related to `upit`, filtered by source and a specific chunk range.
+        Namespace for store can be different than for the original search.
+    
+        :param upit: Query string.
+        :param source_result: source to filter the search results.
+        :param chunk: Target chunk number.
+        :param razmak: Range to consider around the target chunk.
+        :param top_k: Number of top results to return.
+        :param filter: Additional filter criteria for the query.
+        :return: Concatenated page content of the search results.
+        """
+        
+        manji = chunk - razmak
+        veci = chunk + razmak
+    
+        filter_criteria = filter or {}
+        filter_criteria = {
+            'source': source_result,
+            '$and': [{'chunk': {'$gte': manji}}, {'chunk': {'$lte': veci}}]
+        }
+        
+        
+        doc_result, prompt_tokens = self.hybrid_query(upit, top_k=top_k, filter=filter_criteria, namespace=self.namespace)
+
+        # Sort the doc_result based on the 'chunk' value
+        sorted_doc_result = sorted(doc_result, key=lambda document: document.get('chunk', float('inf')))
+
+        # Generate the result string
+        result = " ".join(document.get('page_content', '') for document in sorted_doc_result)
+    
+        return result, prompt_tokens
+
+
+
+
+
+
+
+# tokenizacija za streaming
+def num_tokens_from_messages(messages, model="gpt-4-turbo"):
+    """
+    Return the number of tokens used by a list of messages.
+    """
+    
+    try:
+        encoding = tiktoken.encoding_for_model(model)
+    except KeyError:
+        print("Warning: model not found. Using cl100k_base encoding.")
+        encoding = tiktoken.get_encoding("cl100k_base")
+
+    tokens_per_message = 4  # every message follows <|start|>{role/name}\n{content}<|end|>\n
+    tokens_per_name = -1  # if there's a name, the role is omitted
+    num_tokens = 0
+
+    if type(messages) == list:
+        for message in messages:
+            num_tokens += tokens_per_message
+            for key, value in message.items():
+                num_tokens += len(encoding.encode(value))
+                if key == "name":
+                    num_tokens += tokens_per_name
+        num_tokens += 3  # every reply is primed with <|start|>assistant<|message|>
+    elif type(messages) == str:
+        num_tokens += len(encoding.encode(messages))
+    return num_tokens
+
+
+
+# hybrid (openai no streming)
+llm = ChatOpenAI(api_key=openai_api_key, model="gpt-4-turbo")
+
+total_prompt = 0
+total_completion = 0
+# total_emb_prompt = 0
+# emb_prompt_tokens = 0
+
+
+
+processor = HybridQueryProcessor()
+
+
+
+
+
+
+
+
+
 def main():
     if "username" not in st.session_state:
         st.session_state.username = "positive"
     if deployment_environment == "Azure":    
         st.session_state.username = read_aad_username()
+
+
+
     elif deployment_environment == "Windows":
         st.session_state.username = "lokal"
     elif deployment_environment == "Streamlit":
@@ -100,11 +413,12 @@ def main():
                 else:         
                     with st.chat_message(message["role"], avatar=avatar_sys):
                             st.markdown(message["content"])
+
     # Main conversation UI
     if prompt := st.chat_input("Kako vam mogu pomoci?"):
     
         # Original processing to generate complete_prompt
-        context, scores = processor.process_query_results(prompt)
+        context, scores, emb_prompt_tokens = processor.process_query_results(prompt)
         complete_prompt = st.session_state.rag_answer_reformat.format(prompt=prompt, context=context)
         # Append only the user's original prompt to the actual conversation log
         st.session_state.messages[current_thread_id].append({"role": "user", "content": prompt})
@@ -117,6 +431,10 @@ def main():
         temp_messages = st.session_state.messages[current_thread_id].copy()
         temp_messages[-1] = {"role": "user", "content": complete_prompt}  # Replace last message with enriched context
     
+
+        system_message = {"role": "system", "content": st.session_state.sys_ragbot}
+        user_message = {"role": "user", "content": prompt}
+
         # Generate and display the assistant's response using the temporary messages list
         with st.chat_message("assistant", avatar=avatar_ai):
             message_placeholder = st.empty()
@@ -131,6 +449,21 @@ def main():
                 message_placeholder.markdown(full_response + "▌")
             message_placeholder.markdown(full_response)
         
+
+        total_prompt = 0
+        total_completion = 00
+
+        tiktoken_prompt = [system_message, user_message]
+        tiktoken_prompt_tokens = num_tokens_from_messages(tiktoken_prompt)
+        tiktoken_completion_tokens = num_tokens_from_messages(full_response)
+
+        total_prompt += tiktoken_prompt_tokens
+        total_completion += tiktoken_completion_tokens
+        print("")
+        print(f"- Tiktoken Prompt tokens: {tiktoken_prompt_tokens}")
+        print(f"- Tiktoken Completion tokens: {tiktoken_completion_tokens}")
+
+
         # Append assistant's response to the conversation
         st.session_state.messages[current_thread_id].append({"role": "assistant", "content": full_response})
 
