1ecd1f3cbbbba84593c3b509e94cb6f9c0fb2577
nemper <nemanja.perun98@gmail.com>
2024-09-19 15:19:52 +0200

    texts

--- old-custom-lib/setup.py
+++ old-custom-lib/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="myfunc",
-    version="2.0.82a",
+    version="2.0.82b",
     packages=find_packages(),
     install_requires=[
         'aiohttp==3.9.5',
